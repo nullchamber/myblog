6105376914f47b42f9d0ce9bdb1ddf3a5f2062e5
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-03-08 00:46:28 -0600

    absolute url method for posts

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 # Create your models here.
 
 
@@ -43,3 +45,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
