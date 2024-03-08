72bf29b36742c3c2139bcfbe88e13553dcc832b2
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-03-08 00:56:46 -0600

    pagination for posts

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import Post
 
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
+    # posts = paginator.page(page_number)
+    # return render(
+    #     request,
+    #     'blog/post/list.html',
+    #     {'posts': posts}
+    # )
+
 
 
 # def post_detail(request, id):
