47a637ebc2889f9b6f17c2f4de42606a39b17449
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-03-08 00:50:35 -0600

    SEO friendly URLs

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -19,7 +19,11 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    # slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+    )
     author = models.ForeignKey(
         User,
         related_name='blog_posts',
@@ -47,4 +51,12 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
