57ce2cfbe5719b0ce4c04cd9fe57f7f9b7dcb8c6
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-03-09 00:39:32 -0600

    search uses trigram similarity

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,7 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
+from django.contrib.postgres.search import TrigramSimilarity
 
 from taggit.models import Tag
 
@@ -159,6 +160,29 @@ def post_comment(request, post_id):
 
 
 
+# def post_search(request):
+    # form = SearchForm()
+    # query = None
+    # results = []
+    # if 'query' in request.GET:
+    #     form = SearchForm(request.GET)
+    #     if form.is_valid():
+    #         query = form.cleaned_data['query']
+    #         search_vector = SearchVector('title', 'body')
+    #         search_query = SearchQuery(query)
+    #         results = Post.published.annotate(
+    #             search=search_vector,
+    #             rank=SearchRank(search_vector, search_query)
+    #         ).filter(search=search_query).order_by('-rank')
+    # return render(
+    #     request,
+    #     'blog/post/search.html',
+    #     {
+    #         'form': form,
+    #         'query': query,
+    #         'results': results
+    #     }
+    # )
 def post_search(request):
     form = SearchForm()
     query = None
@@ -168,14 +192,14 @@ def post_search(request):
         if form.is_valid():
             query = form.cleaned_data['query']
             results = Post.published.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+                similarity=TrigramSimilarity('title', query)
+            ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(
         request,
         'blog/post/search.html',
         {
             'form': form,
-            'query': query,
-            'results': results
+            'results': results,
+            'query': query
         }
     )
